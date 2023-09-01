7d78fe5caf65caa1cdf17ab77ded6e2bc4adc9ac
udinantonn@gmail.com <vagrant@ubuntu2010.localdomain>
2023-09-01 16:43:47 +0000

    test_jenkinsfile_final#1.1

--- script_dockerhub.sh
+++ script_dockerhub.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Проверяем наличие файла .env
+
 if [ -f .env ]; then
-    # Загружаем переменные из файла .env
     source .env
     
-    # Выполняем логин
     echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
     
-    # Проверяем статус успешного логина
     if [ $? -eq 0 ]; then
         echo "Login Succeeded"
     else
--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Чтение содержимого файла index.html
+#is a test for demonstration, banning the use of numbers in the index.html
 html_content=$(cat index.html)
 
-# Поиск цифр внутри тегов <h1> и <p> с использованием grep
 if echo "$html_content" | grep -qE '<h1>.*[0-9].*</h1>|<p>.*[0-9].*</p>'; then
     echo "fail"
 else
